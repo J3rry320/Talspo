3f5e2bec3122ad70a40394805f01ad17985e7785
Jerry Satpathy <rutu.satpathy44@gmail.com>
2018-05-25 01:20:55 +0530

    Initial Commit

---
+++ Scripts/script1.js
@@ -0,0 +1,33 @@
+
+document.addEventListener("DOMContentLoaded",()=>{
+  var Paralxinstances = M.Parallax.init(document.querySelectorAll('.parallax'), {
+    
+  })
+  var Caruselinstances = M.Carousel.init(document.querySelectorAll('.carousel'),{
+    shift:10,
+  
+    padding:5
+  });
+  var instances = M.Sidenav.init(document.querySelectorAll('.sidenav'), {
+    inDuration:200
+  });
+  var typed = new Typed("#typewritter", {
+    strings: ["Get Skills That You Deserve", "Explore,Learn and Develop","Be the Best At What You Are"],
+    smartBackspace: true, // Default value,
+    typeSpeed:150,
+    backSpeed:75,
+ 
+    loop:true
+  });
+
+})
+
+$(document).ready(()=>{
+
+if($("#IntroPage").scrollTop()>140){
+$("#Navbar").removeClass("hidden")
+}
+else{
+  $("#Navbar").addClass("hidden")
+}
+})
